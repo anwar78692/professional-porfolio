66d75433673435beb0eff4d2f62c1bb1e1c53957
anwarahmad <anwar15298@gmail.com>
2024-08-31 00:23:35 +0530

    fix issur for design

--- app/components/StarsBackground.js
+++ app/components/StarsBackground.js
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useEffect } from 'react';
@@ -6,19 +5,22 @@ import { useEffect } from 'react';
 const StarsBackground = () => {
   useEffect(() => {
     const rainCount = 40; // Number of raindrops
+    const raindropsContainer = document.createElement('div');
+    raindropsContainer.className = 'raindrops-container';
+    document.body.appendChild(raindropsContainer);
+
     for (let i = 0; i < rainCount; i++) {
       const raindrop = document.createElement('div');
       raindrop.className = 'raindrop';
-      raindrop.style.left = `${Math.random() * 100}vw`; 
-      raindrop.style.animationDuration = `${Math.random() * 2 + 2}s`; 
-      raindrop.style.animationDelay = `${Math.random() * 5}s`; 
-      document.body.appendChild(raindrop);
+      raindrop.style.left = `${Math.random() * 100}vw`;
+      raindrop.style.animationDuration = `${Math.random() * 2 + 2}s`; // Raindrop falls in 2 to 4 seconds
+      raindrop.style.animationDelay = `${Math.random() * 2}s`; // Delay before each raindrop starts falling
+      raindropsContainer.appendChild(raindrop);
     }
 
     // Clean up the raindrops on component unmount
     return () => {
-      const raindrops = document.querySelectorAll('.raindrop');
-      raindrops.forEach((raindrop) => raindrop.remove());
+      raindropsContainer.remove();
     };
   }, []);
 
@@ -26,4 +28,3 @@ const StarsBackground = () => {
 };
 
 export default StarsBackground;
-
